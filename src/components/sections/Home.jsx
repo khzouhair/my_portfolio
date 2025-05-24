719e13ea32d88c8d0ce4f8aadb91ba5a90e21a2c
Khadija Zouhair <147002128+khzouhair@users.noreply.github.com>
2025-05-24 22:38:34 +0100

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -83,7 +83,7 @@ export const Home = () => {
 
             {/* Bouton Télécharger le CV */}
             <a
-              href="/my_portfolio/Khadija_Zouhair_CV.pdf"
+              href={`${import.meta.env.BASE_URL}Khadija_Zouhair_CV.pdf`}
               download="Khadija_Zouhair_CV.pdf"
               className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
             >
