a620db3ae573f2c407a5aeefc834cab245901cb1
Khadija Zouhair <147002128+khzouhair@users.noreply.github.com>
2025-05-24 07:18:36 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+{/*import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -7,3 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/pedrotech-portfolio",
 });
+*/}
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: '/my_portfolio/', // Remplace "portfolio" par le nom de ton dépôt si différent
+  plugins: [react()],
+})
